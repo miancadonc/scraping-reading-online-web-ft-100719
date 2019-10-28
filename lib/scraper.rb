9d6357408e4edca9c09d3346f7f0868806035ac4
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-28 07:04:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
-courses = doc.css(".link-3R0TyF")
+courses = doc.css(".dropdown-2S9KUy.expandRight-aVNzJa")css(".link-3R0TyF")
 
 courses.each do |course|
   puts course.text
