32411382bcb12e1534592751daf43116a27d2526
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-28 07:00:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
-courses = doc.css(".link-3r0TyF")
+courses = doc.css(".link-3R0TyF")
 
 courses.each do |course|
   puts course.text.strip
